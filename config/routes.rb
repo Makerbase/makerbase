ef107d1b0c81b80c16dbccf32980e3dcbb98b063
massud <massudgnan@gmail.com>
2015-05-19 12:38:27 +0100

    first test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+  resources :posts do
+    resources :comments
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
---
+++ spec/features/comments_feature_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'comments' do
+  before {Post.create title: 'First Post'}
+
+  scenario 'allow users to leave a comment on a post' do
+    visit '/'
+    click_link 'Comment First Post'
+    fill_in "Comments", with: "cool link"
+    click_button 'Add Comment'
+
+    expect(current_path).to eq '/'
+    expect(page).to have_content('cool link')
+  end
+
+end
