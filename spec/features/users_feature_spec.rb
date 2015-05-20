df85ed3ed7db267197777f6b6cab6d33ca5b9d1e
Maciej Kurek <maciejk77@gmail.com>
2015-05-20 11:40:30 +0100

    closes #49

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -3,13 +3,18 @@ require 'rails_helper'
 feature 'users' do
   context 'when not signed in' do
     scenario 'should see link to sign in' do
-      visit '/'
+      visit root_path
       expect(page).to have_link 'Sign in with Github'
     end
 
     scenario 'redirected to home when trying to access other pages' do
-      visit '/posts'
+      visit posts_path
       expect(current_path).to eq '/'
+    end
+
+    scenario 'should not see link to posts' do
+      visit root_path
+      expect(page).not_to have_link 'Posts'
     end
   end
 
@@ -22,6 +27,8 @@ feature 'users' do
         :info => { :email => 'sanj@sanj.com', :name => 'sanj' }
         # etc.
       })
+      visit root_path
+      click_link 'Sign in with Github'
     end
 
     after(:each) do
@@ -29,9 +36,22 @@ feature 'users' do
     end
 
     scenario 'can see posts page' do
-      visit root_path
-      click_link 'Sign in with Github'
       visit posts_path
+      expect(current_path).to eq posts_path
     end
+
+    scenario 'should see link to posts' do
+      expect(page).to have_link 'Posts'
+    end
+
+    scenario 'should not see link to sign in' do
+      expect(page).not_to have_link 'Sign in with Github'
+    end
+
+    scenario 'can go to post page' do
+      click_link('Posts')
+      expect(current_path).to eq posts_path
+    end
+
   end
 end
