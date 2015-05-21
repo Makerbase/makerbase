9244e659f1b4cecb1c5d1407f5c35fe9f6af1bbb
jaymuk <jaymuk.business@gmail.com>
2015-05-21 10:13:19 +0100

    fixes github authentication errors

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     # You need to implement the method below in your model (e.g. app/models/user.rb)
+    # byebug
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
@@ -8,7 +9,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, :kind => "github") if is_navigational_format?
     else
       session["devise.github_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      redirect_to root_path
+      flash[:notice] = 'Github log in failed'
     end
   end
 end
