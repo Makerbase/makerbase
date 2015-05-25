f57b11d94e3dd089f7970af0dde0b31dbce12e03
massud <massudgnan@gmail.com>
2015-05-25 16:09:23 +0100

    heroku issues resolved

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,15 +13,11 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :uid, :name
 
   def self.from_omniauth(auth)
-    p auth
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.password = Devise.friendly_token[0,20]
-      # byebug
       user.name = auth.info.nickname   # uses github username
       user.email = auth.info.email.length > 0 ? auth.info.email : "#{auth.info.nickname}@mailinator.com"
       user.image = auth.image
-      p '******'
-      p user
     end
   end
 
