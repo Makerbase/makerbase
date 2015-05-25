8f3be92f559d3b55932e3de3bf43c1ae8f933ccb
massud <massudgnan@gmail.com>
2015-05-25 15:37:43 +0100

    change user model to use first or initialize

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,6 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    p @user
 
     if @user.persisted? && authenticate_organization
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
