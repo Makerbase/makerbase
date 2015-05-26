1974b207d8f1e54fd089ef3435d0298029bb44f8
jaymuk <jaymuk.business@gmail.com>
2015-05-26 18:55:10 +0100

    can see list of users names. Tests passing. very basic atm, need other attributes of user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get '/users', to: 'users#index', as: 'users'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   root 'welcome#index'
