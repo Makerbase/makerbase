e3f5b9292a24722d50baef6743ed9aabe34b2425
sanjsanj <sanjsanj@hotmail.com>
2015-05-27 09:07:20 +0100

    closes #103 - store user gitpage, redirect to from avatar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150526085955) do
+ActiveRecord::Schema.define(version: 20150527074712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 20150526085955) do
     t.string   "uid"
     t.string   "name"
     t.string   "image"
+    t.string   "gitpage"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
